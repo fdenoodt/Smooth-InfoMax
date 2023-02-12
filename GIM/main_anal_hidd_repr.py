0c701ccc74efa1d9383f3d070b9b41562669a490
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2023-02-12 19:34:36 +0100

    main_anal: save encodings + generate 2d visualisation

--- GIM/main_anal_hidd_repr.py
+++ GIM/main_anal_hidd_repr.py
@@ -9,6 +9,10 @@ from arg_parser import arg_parser
 from data import get_dataloader
 import numpy as np
 import random
+import cv2
+import matplotlib.cm as cm
+
+log_path = "analyse_hidden_repr/"
 
 random.seed(0)
 
@@ -17,6 +21,78 @@ if(True):
     from helper_functions import *
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+def visualise_2d_tensor(tensor, GIM_model_name, target_dir, name):
+    nd_arr = tensor.to('cpu').numpy()
+    nb_channels, length = nd_arr.shape
+
+    nd_arr_flat = nd_arr.flatten()  # (nb_channels * length)
+    s = nd_arr_flat / np.max(nd_arr_flat)
+    xs = np.repeat(np.arange(0, nb_channels, 1), length)  # channels
+    ys = np.tile(np.arange(0, length, 1), nb_channels)  # length
+
+    fig, ax = plt.subplots(figsize=(20, 20))
+    ax.scatter(ys, xs, s=1000*(s**4), marker="s", c='orange', alpha=0.3)
+    ax.set_aspect('equal')
+    ax.set_xlabel('Signal length')
+    ax.set_ylabel('Channels')
+    ax.set_title(f'Hidden representation of the audio signal - {GIM_model_name} - {name}')
+
+    # Show the plot
+    plt.savefig(f"{target_dir}/{name}.png")
+    plt.show()
+
+
+def _save_encodings(target_dir, encoder, data_loader):
+    for idx, (batch_org_audio, filenames, _, _) in enumerate(iter(data_loader)):
+        batch_org_audio = batch_org_audio.to(device)
+        batch_enc_audio = encoder(batch_org_audio)
+
+        torch.save(batch_enc_audio, f"{target_dir}/batch_encodings_{idx}.pt")
+        torch.save(filenames, f"{target_dir}/batch_filenames_{idx}.pt")
+
+
+def generate_and_save_encodings(encoder, train_loader, test_loader, GIM_model_name):
+    target_dir = f"{log_path}/hidden_repr/{GIM_model_name}/"
+    train_dir = f"{target_dir}/train"
+    test_dir = f"{target_dir}/test/"
+    create_log_dir(train_dir)
+    create_log_dir(test_dir)
+
+    _save_encodings(train_dir, encoder, train_loader)
+    _save_encodings(test_dir, encoder, test_loader)
+    
+    # visualise_2d_tensor(enc_audio[0], "01GIM_L3", f"{name}_encodings")
+
+def _generate_visualisations(data_dir, GIM_model_name, target_dir):
+    # iterate over files in train_dir
+    for file in os.listdir(data_dir): # Generated via copilot
+        if file.endswith(".pt") and file.startswith("batch_encodings"):
+            # load the file
+            batch_encodings = torch.load(f"{data_dir}/{file}")
+            batch_filenames = torch.load(f"{data_dir}/{file.replace('encodings', 'filenames')}")
+            for idx, (enc, name) in enumerate(zip(batch_encodings, batch_filenames)):
+                name = name.split("_")[0] # eg: babugu_1 -> babugu
+                visualise_2d_tensor(enc, GIM_model_name, target_dir, f"{name}")
+
+def generate_visualisations(GIM_model_name):
+    saved_files_dir = f"{log_path}/hidden_repr/{GIM_model_name}/"
+    train_dir = f"{saved_files_dir}/train"
+    test_dir = f"{saved_files_dir}/test/"
+
+    target_dir = f"{log_path}/hidden_repr_vis/{GIM_model_name}/"
+    train_vis_dir = f"{target_dir}/train"
+    test_vis_dir = f"{target_dir}/test/"
+    create_log_dir(train_vis_dir)
+    create_log_dir(test_vis_dir)
+
+    _generate_visualisations(train_dir, GIM_model_name, train_vis_dir)
+    _generate_visualisations(test_dir, GIM_model_name, test_vis_dir)
+
+
+                
+                # {log_path}/hidden_repr_vis/
 
 
 if __name__ == "__main__":
@@ -32,69 +108,22 @@ if __name__ == "__main__":
     opt['batch_size'] = 64 + 32
     opt['batch_size_multiGPU'] = opt['batch_size']
 
-    create_log_dir(opt['log_path'])
-
     logs = logger.Logger(opt)
 
     # load the data
-    train_loader, _, test_loader, _ = get_dataloader.\
-        get_de_boer_sounds_decoder_data_loaders(opt)
+    # train_loader, _, test_loader, _ = get_dataloader.\
+    #     get_de_boer_sounds_decoder_data_loaders(opt)
 
-    encoder = GIM_Encoder(
-        opt, layer_depth=3, path="DRIVE LOGS/03 MODEL noise 400 epochs/logs/audio_experiment/model_360.ckpt")
+    # layer_depth = 3
+    # encoder = GIM_Encoder(opt, layer_depth=layer_depth, path="DRIVE LOGS/03 MODEL noise 400 epochs/logs/audio_experiment/model_360.ckpt")
 
-    (org_audio, _, _, _) = next(iter(test_loader))
-    org_audio = org_audio.to(device)
-    enc_audio = encoder(org_audio).to(device)
+    # generate_and_save_encodings(encoder, train_loader, test_loader, "01GIM_L3")
+    generate_visualisations("01GIM_L3")
 
-    # torch.cuda.empty_cache()
 
-    # %%
-    log_path = "analyse_hidden_repr/hidden_repr_vis/01GIM_L3"
-    import cv2
-    import matplotlib.cm as cm
 
-    def visualise_3d_tensor(tensor, name):
-        nd_arr = tensor.to('cpu').numpy()
-        nb_channels, length = nd_arr.shape
-
-        nd_arr_flat = nd_arr.flatten()  # (nb_channels * length)
-        s = nd_arr_flat / np.max(nd_arr_flat)
-        xs = np.repeat(np.arange(0, nb_channels, 1), length)  # channels
-        ys = np.tile(np.arange(0, length, 1), nb_channels)  # length
-
-        fig, ax = plt.subplots(figsize=(20, 20))
-        ax.scatter(ys, xs, s=1000*(s**4), marker="s", c='orange', alpha=0.3)
-        ax.set_aspect('equal')
-        ax.set_xlabel('Signal length')
-        ax.set_ylabel('Channels')
-        ax.set_title('Hidden representation of the audio signal - Layer 3')
-
-        # Show the plot
-        plt.savefig(f"{log_path}/{name}.png")
-        plt.show()
-
-    # tensor = torch.zeros(3, 256, 256)
-    # tensor[0, :, :] = 1
-    # enc_audio.shape = (96, 512, 256)
-    visualise_3d_tensor(enc_audio[0], "test")
+    torch.cuda.empty_cache()
 
     # %%
-    import matplotlib.pyplot as plt
-    import numpy as np
-
-    # Generate a 2D numpy array
-    arr = np.random.rand(10, 2)
-
-    # Plot the array as a scatter plot
-    plt.scatter(arr[:, 0], arr[:, 1], s=arr[:, 1]*100)
-
-    # Add labels and title to the plot
-    plt.xlabel('X values')
-    plt.ylabel('Y values')
-    plt.title('Scatter plot with size based on value in the 2D array')
-
-    # Show the plot
-    plt.show()
-
-# %%
+    # visualise_3d_tensor(enc_audio[0], "test")
+    # %%
