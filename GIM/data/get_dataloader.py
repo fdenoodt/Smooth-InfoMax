206fa117ad0e1bb207752d4d1704b643a391a5af
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2023-02-17 12:01:33 +0100

    dataloader shuffle option

--- GIM/data/get_dataloader.py
+++ GIM/data/get_dataloader.py
@@ -138,7 +138,7 @@ def get_de_boer_sounds_data_loaders(opt):
     return train_loader, train_dataset, test_loader, test_dataset
 
 
-def get_de_boer_sounds_decoder_data_loaders(opt):
+def get_de_boer_sounds_decoder_data_loaders(opt, shuffle=True):
     """
     creates and returns the Libri dataset and dataloaders,
     either with train/val split, or train+val/test split
@@ -161,7 +161,7 @@ def get_de_boer_sounds_decoder_data_loaders(opt):
     train_loader = torch.utils.data.DataLoader(
         dataset=train_dataset,
         batch_size=opt["batch_size_multiGPU"],
-        shuffle=True,
+        shuffle=shuffle,
         drop_last=True,
         num_workers=num_workers,
     )
@@ -178,7 +178,7 @@ def get_de_boer_sounds_decoder_data_loaders(opt):
     test_loader = torch.utils.data.DataLoader(
         dataset=test_dataset,
         batch_size=opt["batch_size_multiGPU"],
-        shuffle=True,
+        shuffle=shuffle,
         drop_last=True,
         num_workers=num_workers,
     )
