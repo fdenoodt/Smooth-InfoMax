61a562c8fe6f12df631d48857d905bcf7ca2fecb
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2023-05-17 22:50:54 +0200

    Update generate_loss_graphs.py

--- GIM/generate_loss_graphs.py
+++ GIM/generate_loss_graphs.py
@@ -28,36 +28,44 @@ import tikzplotlib
 #     plt.show()
 
 
-root = r"D:\thesis_logs\logs\good models\de_boer_TWO_MODULE_V3_dim32_kld_weight=0"
+root = r"D:\thesis_logs\logs\GIM_DECODER_simple_v2_experiment\MEL_SPECTR_n_fft=4096 !!\lr_0.0050000\GIM_L1"
+root = r"D:\thesis_logs\logs\de_boer_TWO_MODULE_V3_dim32_kld_weight=0.0033 !!\DECODER\MEL_SPECTR_n_fft=4096\lr_0.0050000\GIM_L1"
 
 # iterate over all files in the folder
 for file in os.listdir(root):
-    if file.endswith(".csv") and file.startswith("train_loss"):
+    if file.endswith(".csv") and file.startswith("train_loss"): # first starts with checks VGIM, second checks decoder
           # get the index of the file
           idx = file.split("_")[2].split(".")[0]
           # load in the train and val loss
           train_loss = np.loadtxt(os.path.join(root, f"train_loss_{idx}.csv"))
           val_loss = np.loadtxt(os.path.join(root, f"val_loss_{idx}.csv"))
-          # draw the loss curve
-          # draw_loss_curve(train_loss, val_loss)
-          lst_iter = np.arange(len(train_loss))
-          plt.plot(lst_iter, np.array(train_loss), "-b", label="train loss")
+    elif file.endswith(".csv") and file.startswith("training_loss"):
+            train_loss = np.loadtxt(os.path.join(root, "training_loss.csv"))
+            val_loss = np.loadtxt(os.path.join(root, "validation_loss.csv"))
+            idx = "0"
+    else:
+        continue
 
-          lst_iter = np.arange(len(val_loss))
-          plt.plot(lst_iter, np.array(val_loss), "-r", label="val loss")
+    # draw the loss curve
+    # draw_loss_curve(train_loss, val_loss)
+    lst_iter = np.arange(len(train_loss))
+    plt.plot(lst_iter, np.array(train_loss), "-b", label="train loss")
 
-          plt.xlabel("epoch")
-          plt.ylabel("loss")
-          plt.legend(loc="upper right")
+    lst_iter = np.arange(len(val_loss))
+    plt.plot(lst_iter, np.array(val_loss), "-r", label="val loss")
 
-          # save image
-          # plt.savefig(os.path.join(self.logging_path, "loss.png"))
-          # plt.close()
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+    plt.legend(loc="upper right")
 
-          # grid on
-          plt.grid(False)
+    # save image
+    # plt.savefig(os.path.join(self.logging_path, "loss.png"))
+    # plt.close()
 
-          tikzplotlib.save(os.path.join(root, f"loss_{idx}.tex"))
-          plt.show()
+    # grid on
+    plt.grid(False)
+
+    tikzplotlib.save(os.path.join(root, f"loss_{idx}.tex"))
+    plt.show()
 
           # plt.close()
