9faccbbac8a7bccdd90996febd73e09384a8d394
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2024-03-07 20:50:15 +0100

    Create logistic_regression_vowels.py

---
+++ linear_classifiers/logistic_regression_vowels.py
@@ -0,0 +1,7 @@
+# python -m linear_classifiers.logistic_regression_vowels temp sim_audio_distr_true --overrides syllables_classifier_config.encoder_num=9
+from linear_classifiers.logistic_regression import main
+
+if __name__ == "__main__":
+    wandb, wandb_is_on = main(syllables=False)  # syllables classification
+    if wandb_is_on:
+        wandb.finish()
