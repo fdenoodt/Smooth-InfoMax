c6553958c63c8748c4b3a81698602ec609776545
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2024-03-07 20:38:25 +0100

    temporarily set num_epochs=5

--- config_code/sim_setup.py
+++ config_code/sim_setup.py
@@ -75,7 +75,7 @@ class SIMSetup:
 
         self.ENCODER_CONFIG = EncoderConfig(
             start_epoch=0,
-            num_epochs=1_000,
+            num_epochs=5,
             negative_samples=10,
             subsample=True,
             architecture=ARCHITECTURE,
@@ -87,7 +87,7 @@ class SIMSetup:
         )
 
         self.CLASSIFIER_CONFIG_PHONES = ClassifierConfig(
-            num_epochs=20,
+            num_epochs=5,
             learning_rate=1e-4,  # = 0.0001
             # Deep copy of the dataset, to avoid changing the original dataset
             dataset=DATASET.__copy__(),
@@ -97,7 +97,7 @@ class SIMSetup:
         self.CLASSIFIER_CONFIG_PHONES.dataset.batch_size = 8
 
         self.CLASSIFIER_CONFIG_SPEAKERS = ClassifierConfig(
-            num_epochs=50,
+            num_epochs=5,
             learning_rate=1e-3,  # = 0.001
             dataset=DATASET.__copy__(),
             # For loading a specific model from a specific epoch, to use by the classifier
@@ -106,7 +106,7 @@ class SIMSetup:
         self.CLASSIFIER_CONFIG_SPEAKERS.dataset.batch_size = 64
 
         self.CLASSIFIER_CONFIG_SYLLABLES = ClassifierConfig(
-            num_epochs=50,
+            num_epochs=5,
             learning_rate=1e-3,  # = 0.001
             dataset=DataSetConfig(
                 dataset=dataset,
@@ -121,7 +121,7 @@ class SIMSetup:
         )
 
         self.DECODER_CONFIG = DecoderConfig(
-            num_epochs=200,
+            num_epochs=5,
             learning_rate=2e-4,
             dataset=DataSetConfig(
                 dataset=dataset,
