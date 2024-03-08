3e6858a9e0f447f0c4b64292b15870c39424c234
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2024-03-08 17:17:33 +0100

    plot 3d scatter plot

--- post_hoc_analysis/interpretability/t_sne.py
+++ post_hoc_analysis/interpretability/t_sne.py
@@ -18,7 +18,8 @@ import torch
 import torch.nn as nn
 import time
 import numpy as np
-
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import os
 import wandb
 
@@ -124,6 +125,44 @@ def _get_data_from_loader(loader, encoder: FullModel, opt: OptionsConfig, final_
     return all_audio, all_labels
 
 
+def scatter_3d(x, y, z, labels, title, dir, file, show, wandb_is_on):
+    # x, y, z: (batch_size, seq_len)
+    # labels: (batch_size,) so must copy the labels for each seq_len
+    if len(x.shape) > 1:
+        labels = np.repeat(labels, x.shape[1])
+
+    x, y, z = x.flatten(), y.flatten(), z.flatten()
+
+    # limit to 1000 points
+    if x.shape[0] > 1000:
+        indices = random.sample(range(x.shape[0]), 1000)
+        x, y, z, labels = x[indices], y[indices], z[indices], labels[indices]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    scatter = ax.scatter(x, y, z, c=labels, cmap='viridis', s=2)
+
+    # set x, y, z limits between -3 and 3
+    ax.set_xlim(-1.5, 1.5)
+    ax.set_ylim(-1.5, 1.5)
+    ax.set_zlim(-1.5, 1.5)
+
+    ax.set_title(title)
+    plt.colorbar(scatter)
+
+    if show:
+        plt.show()
+
+    fig.savefig(f"{dir}/{file}.png")
+    fig.savefig(f"{dir}/{file}.pdf")
+
+    if wandb_is_on:
+        wandb.log({f"LatSpace/3D_latent_space_{file}": [wandb.Image(f"{dir}/{file}.png")]})
+
+    return f"{dir}/{file}.png"
+
+
 def main():
     opt: OptionsConfig = get_options()
 
@@ -175,6 +214,16 @@ def main():
     lr, n_iter, perplexity = ('auto', 1000, int(float(np.sqrt(n))))
     plot_tsne(opt, all_audio_mean, all_labels, f"MEAN_SIM_{lr}_{n_iter}_{perplexity}",
               lr=lr, n_iter=n_iter, perplexity=perplexity, wandb_is_on=wandb_is_on)
+
+    data_config.dataset.split_in_syllables = 'vowels'
+    train_loader_syllables, _, test_loader_syllables, _ = get_dataloader.get_dataloader(data_config)
+    all_audio, all_labels = _get_data_from_loader(train_loader_syllables, context_model.module, opt, "final")
+    n = all_labels.shape[0]  # sqrt(1920) ~= 44
+
+    _audio_per_channel = np.moveaxis(all_audio, 1, 0)
+    scatter_3d(_audio_per_channel[0], _audio_per_channel[1], _audio_per_channel[2],
+               all_labels, title=f"3D Latent Space of the First Three Dimensions", dir=opt.log_path,
+               file=f"_ 3D latent space idices 0_1_2", show=False, wandb_is_on=wandb_is_on)
 
     # retrieve full data that encoder was trained on
     data_config.split_in_syllables = False
