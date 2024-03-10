c40e1e877c6cf7685ad02b7fa2b685ef4d4b2cb8
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2024-03-10 11:45:18 +0100

    Update vowel_classifier_analysis.py

--- post_hoc_analysis/interpretability/vowel_classifier_analysis.py
+++ post_hoc_analysis/interpretability/vowel_classifier_analysis.py
@@ -171,7 +171,7 @@ def main():
 
     # the classifier is a part of the loss function
     n_labels = 3
-    n_features = opt.encoder_config.architecture.modules[0].regressor_hidden_dim # 512
+    n_features = opt.encoder_config.architecture.modules[0].regressor_hidden_dim # 256, is being multiplied by 2 in Syllables_Loss()
     syllables_loss = Syllables_Loss(opt, hidden_dim=n_features, calc_accuracy=True, bias=False, num_syllables=n_labels)
 
     # Load the trained model
