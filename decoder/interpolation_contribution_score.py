0224cab86996ba2f6ac70ed40b8d023646394347
Anonymous user <fabianbusiness@outlook.com>
2024-06-20 17:58:53 +0200

    speed up InterpolationContributionScore

--- decoder/interpolation_contribution_score.py
+++ decoder/interpolation_contribution_score.py
@@ -17,7 +17,6 @@ class InterpolationContributionScore:
     def _get_two_zs(self, z, filenames, idx1, idx2, print_names: bool = True) -> (np.ndarray, np.ndarray, str, str):
         assert idx1 != idx2
 
-        z = z.cpu().numpy()
         z1 = z[idx1].reshape(1, self.nb_dims, -1)  # (1, 512, 64)
         z2 = z[idx2].reshape(1, self.nb_dims, -1)  # (1, 512, 64)
         z1_file = filenames[idx1]
@@ -37,16 +36,16 @@ class InterpolationContributionScore:
         return z_interpolated
 
     def _interpolate_partial(self, z1, z2, indices, nb_interpolations=10):
-        z_interpolated = np.zeros((nb_interpolations, self.nb_dims, self.latent_nb_frames))
+        z_interpolated = torch.zeros((nb_interpolations, self.nb_dims, self.latent_nb_frames), device=self.opt.device)
         for i in range(nb_interpolations):
             val = i / (nb_interpolations - 1)
-            z_interpolated[i] = z1.copy()
+            z_interpolated[i] = z1.clone()
             z_interpolated[i, indices] = z1[0, indices] * val + z2[0, indices] * (1 - val)
-        # return z_interpolated
+        return z_interpolated
 
         # to tensor
-        z_interpolated = torch.from_numpy(z_interpolated).float().to(self.opt.device)
-        return z_interpolated
+        # z_interpolated = torch.from_numpy(z_interpolated).float().to(self.opt.device)
+        # return z_interpolated
 
     def _dist_after_interpol_important_dims(self, z1, z2, nb_dims_important_dims, max_err: bool = False):
         assert nb_dims_important_dims <= self.nb_dims
@@ -54,10 +53,12 @@ class InterpolationContributionScore:
         z_1_single_timeframe = z1.mean(axis=2)  # (1, 32)
         z_2_single_timeframe = z2.mean(axis=2)  # (1, 32)
 
-        # mse
         mse = (z_1_single_timeframe - z_2_single_timeframe) ** 2
-        dims = np.argsort(mse.squeeze())[::-1][:nb_dims_important_dims]  # in descending order
 
+        # old numpy line:
+        # dims = numpy.argsort(mse.squeeze())[::-1][:nb_dims_important_dims]  # in descending order
+
+        dims = torch.argsort(mse.squeeze(), descending=True)[:nb_dims_important_dims]
         if max_err:
             z2_partial = self._interpolate_partial(z1, z2, dims, 10)[-1]  # take the least interpolated z
         else:
@@ -65,17 +66,14 @@ class InterpolationContributionScore:
                 0]  # take the most interpolated z (same as having a mask)
 
         x2_partial = self.lit_decoder.decoder(z2_partial).squeeze()
-        z2 = torch.from_numpy(z2).float().to(self.opt.device)  # to tensor
+        # z2 = torch.from_numpy(z2).float().to(self.opt.device)  # to tensor
         x2_target = self.lit_decoder.decoder(z2).squeeze()
 
-        # dist = np.abs(x2_partial - x2_target)
-        # dist = dist.mean()
-
         # MSE
-        dist = torch.mean((x2_partial - x2_target) ** 2).item()
+        dist = torch.mean((x2_partial - x2_target) ** 2)
 
-        # assert dist is a scalar, not a tensor
-        assert np.isscalar(dist)
+        # assert dist is a tensor but with a single value
+        assert len(dist.shape) == 0
 
         return dist
 
@@ -109,11 +107,11 @@ class InterpolationContributionScore:
         self.lit_decoder = self.lit_decoder.to(self.opt.device)
         self.lit_decoder.eval()
 
-        _, _, z, filenames = self._get_all_data(self.opt, self.lit_decoder)
+        _, _, z, filenames = self._get_all_data(self.opt, self.lit_decoder)  # z is a tensor
         nb_files = len(filenames)
 
         # calc max error
-        max_error = 0
+        max_error = torch.tensor(0.0, device=self.opt.device)
         for i in range(nb_files):
             for j in range(nb_files):
                 if i != j:
@@ -121,17 +119,17 @@ class InterpolationContributionScore:
                     dist = self._dist_after_interpol_important_dims(z1, z2, 512, max_err=True)
                     max_error += dist
 
-                if j == 10:  # TODO: remove this
-                    break
-            if i == 10:
-                break
+            #     if j == 10:  # TODO: remove this
+            #         break
+            # if i == 10:
+            #     break
 
         max_error /= (nb_files * (nb_files - 1))
         print(f"Max error: {max_error}")
 
         results: Dict[int, float] = {}
         for nb_most_important_dims in [2, 4, 8, 16, 32, 64, 128, 256, 512]:
-            avg_error = 0
+            avg_error = torch.tensor(0.0, device=self.opt.device)
             print(f"nb_most_important_dims: {nb_most_important_dims}")
             for i in range(nb_files):
                 for j in range(nb_files):
@@ -149,6 +147,6 @@ class InterpolationContributionScore:
             # print(f"Unscaled average error for dim {nb_most_important_dims}: {avg_error}")
             # print(f"Scaled average error for dim {nb_most_important_dims}: {avg_error / max_error}")
             normalized_avg_error = avg_error / max_error
-            results[nb_most_important_dims] = normalized_avg_error
+            results[nb_most_important_dims] = normalized_avg_error.item()
 
         return results
