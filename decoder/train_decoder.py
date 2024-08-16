4e09485f544ef85628781fae07ea95d02266297a
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2024-08-16 20:02:04 +0200

    Update train_decoder.py

--- decoder/train_decoder.py
+++ decoder/train_decoder.py
@@ -86,7 +86,6 @@ def main(model_type: ModelType = ModelType.ONLY_DOWNSTREAM_TASK):
                         accelerator="gpu", devices="1",
                         log_every_n_steps=10,  # arbitrary number to avoid warning
                         logger=wandb_logger, callbacks=[callback] if callback is not None else [],
-                        fast_dev_run=True # TODO
                         )
 
     if opt.train:
